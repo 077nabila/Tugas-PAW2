140d624fcf86e4b1d523675cb9c9a8d84b34692f
077nabila <84031548+077nabila@users.noreply.github.com>
2022-03-13 09:03:24 +0700

    Membuat file variable.php

---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php
+    $garis = "------------------------------------------------------------<br>";
+    $a = 4;
+    echo "Nilai a adalah : $a <br>";
+    echo "$garis";
+    $b = 2;
+    echo "Nilai b adalah : $b<br>";
+    echo $garis;
+    $c = 3;
+    echo "Nilai c adalah : $c<br>";
+    $hasil = ($a + $b) * $c;
+    echo "<strong>Maka hasil dari ($a + $b) * $c = $hasil </strong><br>";
+    echo $garis;
+    $hasil = ($a + $b) / $c;
+    echo "<strong>Maka hasil dari ($a + $b) / $c = $hasil </strong><br>";
+    echo $garis;
+    $hasil = ($a - $b) * $c;
+    echo "<strong>Maka hasil dari ($a - $b) * $c = $hasil </strong><br>";
+    echo $garis;
+?>
