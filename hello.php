e1d340e2796e5b1f9d6dc75f2c57cf6060826619
077nabila <84031548+077nabila@users.noreply.github.com>
2022-03-13 10:34:14 +0700

    Menambah keterangan pada command

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 <head>
 </head>
 <body>
-    <h1>Nama : <?php echo "Nabila Regitasyari Irmawan";?><h1> <!--//disini termasuk php didalam html. Fungsi Echo sendiri untuk menampilkan teks ke layar. -->
-    <h1>Nim : <?php echo "200411100077";?><h1>
+    <h1>Nama : <?php echo"<h1>Nabila Regitasyari Irmawan</h1>";// tag h1 berfungsi untuk membuat teks menjadi besar dan tebal
+    <h1>Nim : <?php echo "200411100077";?><h1> <h1> <!--//disini termasuk php didalam html. Fungsi Echo sendiri untuk menampilkan teks ke layar. -->
 </body>
 </html>
