ba8cebf30c7f6dd54dd4687f3e84d95038349c87
077nabila <84031548+077nabila@users.noreply.github.com>
2022-03-13 08:59:06 +0700

    Menambahkan file hello.php

---
+++ hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal Nomor 1</title>
+</head>
+<body>
+    <h1>Nama : <?php echo "Nabila Regitasyari Irmawan";?><h1> <!--//disini termasuk php didalam html. Fungsi Echo sendiri untuk menampilkan teks ke layar. -->
+    <h1>Nim : <?php echo "200411100077";?><h1>
+</body>
+</html>
