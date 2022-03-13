ae20f971b043716c9b65c1f2ebaa9d199d467818
077nabila <84031548+077nabila@users.noreply.github.com>
2022-03-13 09:31:17 +0700

    menghapus beberapa tag

--- hello.php
+++ hello.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Soal Nomor 1</title>
 </head>
 <body>
     <h1>Nama : <?php echo "Nabila Regitasyari Irmawan";?><h1> <!--//disini termasuk php didalam html. Fungsi Echo sendiri untuk menampilkan teks ke layar. -->
